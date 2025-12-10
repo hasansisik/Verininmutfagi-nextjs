c44d7000b4ad092a6b9365cb72f2eb69d9bbdca6
Hasan Şişik <sisik.hsn@gmail.com>
2025-12-10 22:14:10 +0300

    first commit

---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+import NotFound from "@/components/inner-pages/error";
+import Wrapper from "@/layouts/Wrapper";
+
+export const metadata = {
+   title: "Contact SkillGro - Online Courses & Education React Next js Template",
+};
+const page = () => {
+   return (
+      <Wrapper>
+         <NotFound />
+      </Wrapper>
+   )
+}
+
+export default page
+
